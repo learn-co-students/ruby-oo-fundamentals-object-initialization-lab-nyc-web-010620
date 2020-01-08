b429a89ca92848f57efb50e97d5f49855689939c
Ramzy Badawy <Ramzy.Badawy@gmail.com>
2020-01-08 18:51:15 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,22 @@
 class Dog
-    def initialize(name, breed = "Mutt")
-      @name = name
-      @breed = breed
-    end
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
   end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
